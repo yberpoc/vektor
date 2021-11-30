f12e8f4e3f89b9bfa6e269eb0170580c3ff27dd8
Maria <mwehmwmew@yandex.ru>
2021-11-30 08:53:21 +0300

    пожалуйста, больше не обижайте меня, а то оно уйдет совсем

--- local/templates/vektor/footer.php
+++ local/templates/vektor/footer.php
@@ -25,26 +25,22 @@
       <span class="footer__address">614038, Россия, Пермский край, г. Пермь, улица Академика Веденеева, 80а</span>
     </div>
     <div class="footer__col footer__col_menu">
-        <?$APPLICATION->IncludeComponent(
-            "bitrix:menu",
-            "bottom_menu",
-            Array(
-                "ALLOW_MULTI_SELECT" => "N",
-                "CHILD_MENU_TYPE" => "left",
-                "DELAY" => "N",
-                "MAX_LEVEL" => "1",
-                "MENU_CACHE_GET_VARS" => array(""),
-                "MENU_CACHE_TIME" => "3600",
-                "MENU_CACHE_TYPE" => "N",
-                "MENU_CACHE_USE_GROUPS" => "Y",
-                "ROOT_MENU_TYPE" => "bottom",
-                "USE_EXT" => "N"
-            )
-        );?>
-<<<<<<< HEAD
-
-=======
->>>>>>> 840c2c85bc0d8f1db32d1e04081cab82ba9f2686
+        <?$APPLICATION->IncludeComponent("bitrix:menu", "bottom_menu", Array(
+	"ALLOW_MULTI_SELECT" => "N",	// Разрешить несколько активных пунктов одновременно
+		"CHILD_MENU_TYPE" => "left",	// Тип меню для остальных уровней
+		"DELAY" => "N",	// Откладывать выполнение шаблона меню
+		"MAX_LEVEL" => "1",	// Уровень вложенности меню
+		"MENU_CACHE_GET_VARS" => array(	// Значимые переменные запроса
+			0 => "",
+		),
+		"MENU_CACHE_TIME" => "3600",	// Время кеширования (сек.)
+		"MENU_CACHE_TYPE" => "N",	// Тип кеширования
+		"MENU_CACHE_USE_GROUPS" => "Y",	// Учитывать права доступа
+		"ROOT_MENU_TYPE" => "bottom",	// Тип меню для первого уровня
+		"USE_EXT" => "N",	// Подключать файлы с именами вида .тип_меню.menu_ext.php
+	),
+	false
+);?>
       <ul class="footer-social">
         <li class="footer-social__item">
           <a href="" target="_blank" class="footer-social__link">
