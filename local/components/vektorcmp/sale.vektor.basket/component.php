ebe901c3723bafb646c73b8831955326c0fcb3af
Maria <73960907+mwehmwmew@users.noreply.github.com>
2021-12-02 09:38:37 +0500

    Update component.php

--- local/components/vektorcmp/sale.vektor.basket/component.php
+++ local/components/vektorcmp/sale.vektor.basket/component.php
@@ -53,65 +53,13 @@ while ($product = $products_in_cart->Fetch()) {
     }
     $arResultItems[] = $product;
 }
-
-// <получение данных корзины со свойствами товаров из инфоблока>
-$arBasketProps = array();
-$dbBasketItems = CSaleBasket::GetList(
-	array(
-		"NAME" => "ASC",
-		"ID" => "ASC"
-	),
-	array(
-		"FUSER_ID" => CSaleBasket::GetBasketUserID(),
-		"LID" => SITE_ID,
-		"ORDER_ID" => "NULL"
-	),
-	false,
-	false,
-	array(
-		"ID",
-		"CALLBACK_FUNC",
-		"MODULE",
-		"PRODUCT_ID",
-		"QUANTITY",
-		"DELAY",
-		"CAN_BUY",
-		"PRICE",
-		"ORDER_PRICE",
-		"BASE_PRICE",
-		"WEIGHT"
-	)
-);
-
-while ($arItems = $dbBasketItems->Fetch()) {
-	if (strlen($arItems["CALLBACK_FUNC"]) > 0)
-	{
-		CSaleBasket::UpdatePrice($arItems["PRODUCT_ID"]);
-		$arItems = CSaleBasket::GetByID($arItems["ID"]);
-	}
-	$arFilter = array("IBLOCK_ID"=>5, "ID"=>$arItems['PRODUCT_ID'], "ACTIVE_DATE"=>"Y", "ACTIVE"=>"Y");
-	$arSelect = Array("ID", "NAME", "DATE_ACTIVE_FROM", "PREVIEW_PICTURE", "PREVIEW_TEXT", "DETAIL_PAGE_URL");
-	$res = CIBlockElement::GetList(Array(), $arFilter, false, Array("nPageSize"=>50), $arSelect);
-	while ($ob = $res->GetNextElement())
-	{
-		$arFields = $ob->GetFields();
-		$img = CFile::GetPath($arFields["PREVIEW_PICTURE"]);
-		$arFields["PREVIEW_PICTURE"] = $img;
-		$arFields["PRICE"] = $arItems["PRICE"];
-		$arFields["EL_BASKET_ID"] = $arItems["ID"];
-		$arFields["QUANTITY"] = $arItems["QUANTITY"];
-
-		$arBasketProps[] = $arFields;
-	}
-	$arResultItems[] = $arItems;
-}
 echo '<pre>';
 print_r($_REQUEST["ID"]);
 echo '</pre>';
 
 $count = $_REQUEST["quantity"];
 $ID = $_REQUEST["ID"];
-<<<<<<< HEAD
+
 if ($_GET["method"] == 'addQuantity') {
     $arFields = array("QUANTITY" => ++$count);
     CSaleBasket::Update($ID, $arFields);
@@ -125,23 +73,6 @@ if ($_GET["method"] == 'deleteQuantity') {
 if ($_GET["method"] == 'delete') {
     CSaleBasket::Delete($ID);
     header('Location: index.php');
-=======
-switch ($_GET["method"]) {
-	case 'addQuantity':
-		$arFields = array("QUANTITY" => ++$count);
-		CSaleBasket::Update($ID, $arFields);
-		header('Location: /basket/index.php');
-		break;
-	case 'delQuantity':
-		$arFields = array("QUANTITY" => --$count);
-		CSaleBasket::Update($ID, $arFields);
-		header('Location: /basket/index.php');
-		break;
-	case 'deleteProduct':
-		CSaleBasket::Delete($ID);
-		header('Location: /basket/index.php');
-		break;
->>>>>>> a217285b1da4b38643c1fe3fdd6e287a9c6b7d8c
 }
 
 
@@ -156,13 +87,8 @@ $email = $_REQUEST['email'];
 
 
 $arResult = array(
-<<<<<<< HEAD
     "ITEMS" => $arBasket,
     "ERROR" => "",
-=======
-	"ITEMS" => $arBasketProps,
-	"ERROR" => "",
->>>>>>> a217285b1da4b38643c1fe3fdd6e287a9c6b7d8c
 );
 
 
@@ -177,17 +103,13 @@ if ($USER->IsAuthorized()) {
 	}
 }
 if (isset($userID)) {
-<<<<<<< HEAD
     setOrder($userID, $person, $company, $phone, $email); // отправка заказа
-=======
-	setOrder($userID, $person, $company, $phone, $email, $task, $comment, $agreement); // отправка заказа
->>>>>>> a217285b1da4b38643c1fe3fdd6e287a9c6b7d8c
 } else {
 	echo 'ошибка';
 }
 
 // <функция для отправки заказа>
-<<<<<<< HEAD
+
 function setOrder($userID, $person, $company, $phone, $email){
     if (isset($person) && isset($company) && isset($email)){
         $order = Order::create(SITE_ID, $userID);
@@ -212,34 +134,6 @@ function setOrder($userID, $person, $company, $phone, $email){
         $orderId = $order->getId();
         header('Location: /basket/index.php');
     }
-=======
-function setOrder($userID, $person, $company, $phone, $email, $task, $comment, $agreement){
-	if (isset($person) && isset($company) && isset($email)){
-		$order = Order::create(SITE_ID, $userID);
-		$order->setPersonTypeId(2);
-
-		// Создаёт корзину с товаром
-		$basket = Bitrix\Sale\Basket::loadItemsForFUser(Bitrix\Sale\Fuser::getId(), Bitrix\Main\Context::getCurrent()->getSite());
-		$order->setBasket($basket);
-
-		if ($comment) {
-			$order->setField('USER_DESCRIPTION', $comment); // Устанавливаем поля комментария покупателя
-		}
-
-		$propertyCollection = $order->getPropertyCollection();
-
-		$propertyCollection->getItemByOrderPropertyId(12)->setValue($person);
-		$propertyCollection->getItemByOrderPropertyId(13)->setValue($email);
-		$propertyCollection->getItemByOrderPropertyId(14)->setValue($phone);
-		$propertyCollection->getItemByOrderPropertyId(8)->setValue($company);
-		//$propertyCollection->getItemByOrderPropertyId(22)->setValue($task);
-
-		$order->doFinalAction(true);
-		$result = $order->save();
-		$orderId = $order->getId();
-		header('Location: /basket/successful.php');
-	}
->>>>>>> a217285b1da4b38643c1fe3fdd6e287a9c6b7d8c
 }
 // </функция для отправки заказа>
 
